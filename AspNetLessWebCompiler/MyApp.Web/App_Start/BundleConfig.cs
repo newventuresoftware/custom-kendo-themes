e03691d42b71a29612991eb2155e04d706b5b067
Milan Nankov <milan@newventuresoftware.com>
2017-05-04 16:44:15 +0300

    Added Kendo scripts other files

--- AspNetLessWebCompiler/MyApp.Web/App_Start/BundleConfig.cs
+++ AspNetLessWebCompiler/MyApp.Web/App_Start/BundleConfig.cs
@@ -11,6 +11,9 @@ namespace MyApp.Web
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/kendo").Include(
+                        "~/Scripts/kendo/kendo.ui.core.min.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
@@ -24,6 +27,9 @@ namespace MyApp.Web
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/kendo/kendo.custom.common.css",
+                      "~/Content/kendo/kendo.custom.css",
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
         }
